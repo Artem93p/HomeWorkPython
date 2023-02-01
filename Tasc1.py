be66c278a33619cc7a35c9c554a48cb080385e7f
ArtemP <artem93.01@mail.ru>
2023-02-01 23:40:37 +0300

    HomeWork

---
+++ Tasc1.py
@@ -0,0 +1,12 @@
+# Задача 1: Найдите сумму цифр трехзначного числа.
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+print("Введите трехзначное число: ")
+number = int(input())
+sum = 0
+while (number != 0):
+    x = number % 10
+    sum = sum + x
+    number = number // 10
+print(sum)
